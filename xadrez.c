cfc5fe5a6bca272f25714240388af4f5b1b2e34f
aluno2025 <202507053541@alunos.estacio.br>
2025-08-24 00:00:09 +0000

    Nivel Amador: Melhorias e interação com o jogador

--- xadrez.c
+++ xadrez.c
@@ -4,33 +4,150 @@
 
     int main() {
         /*
-        NIVEL INICIANTE:
-        APENAS SIMULANDO OS MOVIMENTOS DE TORRE / BISPO / RAINHA
-        (O aprimoramento da direção sera nas proximas atualizações)
+        NIVEL AMADOR:
+        APENAS SIMULANDO OS MOVIMENTOS DE TORRE / BISPO / RAINHA / CAVALO
+        (Aguarde melhorias nas proximas atualizações)
         */ 
-        
-        /* TORRE COM WHILE
+        printf("                     ##### ATENÇÃO JOGADOR ##### \n");
+        printf("--- ESTE É APENAS UM PROJETO DE SIMULAÇÃO DE MOVIMENTOS DAS PEÇAS ---\n\n");
+        // TORRE COM WHILE
+        int direcaoTorre;
         int i = 1;
+        printf("Em qual direção a Torre deve ir? \n");
+        printf("1.Direita   2.Esquerda   3.Pra frente   4.Pra trás \n");
+        scanf("%d", &direcaoTorre);
+        switch (direcaoTorre)
+        {
+        case 1:
         while (i <= 5){
-        printf("Pra Frente...\n");
+        printf("Direita...\n");
         i++;
         }
-        */
+            break;
         
-        /* BISPO COM DO WHILE
-        int i = 1;
+        case 2:
+        while (i <= 5){
+        printf("Esquerda...\n");
+        i++;
+        }
+            break;
+            
+        case 3:
+        while (i <= 5){
+        printf("Pra frente...\n");
+        i++;
+        }
+        
+        case 4:
+        while (i <= 5){
+        printf("Pra trás...\n");
+        i++;
+        }
+        }
+        printf("\n\n\n");
+
+        // BISPO COM DO WHILE
+        int direcaoBispo;
+        i = 1;
+        printf("Em qual direção o Bispo deve ir? \n");
+        printf("1.Diagonal direita   2.Diagonal esquerda\n");
+        scanf("%d", &direcaoBispo);
+        switch (direcaoBispo)
+        {
+        case 1:
         do {
-        printf("Diagonal... \n");
+        printf("Diagonal direita... \n");
         i++;
         } while (i <=5);
-         */
+            break;
+        
+        case 2:
+        do {
+        printf("Diagonal esquerda... \n");
+        i++;
+        } while (i <=5);
+            break;
+        }
+        printf("\n\n\n");
 
-        /* RAINHA COM FOR
+        // RAINHA COM FOR
+        int direcaoRainha;
+        printf("Em qual direção a Rainha deve ir? \n");
+        printf("1.Direita   2.Esquerda   3.Pra frente \n");
+        printf("4.Pra trás  5.Diagonal direita  6.Diagonal esquerda \n");
+        scanf("%d", &direcaoRainha);
+        switch (direcaoRainha)
+        {
+        case 1:
         for (int i = 1; i <= 8; i++)
         {
-            printf("Pra frente / diagonal / esquerda / direita ...\n");
+        printf("Direita... \n");
         }
-        */
+            break;
+        
+        case 2:
+        for (int i = 1; i <= 8; i++)
+        {
+        printf("Esquerda... \n");
+        }
+            break;
+
+        case 3:
+        for (int i = 1; i <= 8; i++)
+        {
+        printf("Pra frente... \n");
+        }
+            break;
+        case 4:
+        for (int i = 1; i <= 8; i++)
+        {
+        printf("Pra trás... \n");
+        }
+            break;
+
+        case 5:
+        for (int i = 1; i <= 8; i++)
+        {
+        printf("Diagonal direita... \n");
+        }
+            break;
+
+        case 6:
+        for (int i = 1; i <= 8; i++)
+        {
+        printf("6.Diagonal esquerda... \n");
+        }
+            break;        
+        }
+        printf("\n\n\n");
+
+        // CAVALO
+        int direcaoCavalo;
+        printf("Em qual direção o Cavalo deve ir? \n");
+        printf("1.Pra frente / Direita   2.Pra frente / Esquerda \n");
+        scanf("%d", &direcaoCavalo);
+        switch (direcaoCavalo)
+        {
+        case 1:
+         for (int i = 0; i <= 0; i++){
+         for (int j = 0; j <=1; j++)
+         {
+            printf("Cima... \n");
+         }
+            printf("Direita... \n");
+         }
+        break;
+        case 2:
+         for (int i = 0; i <= 0; i++){
+         for (int j = 0; j <=1; j++)
+         {
+            printf("Cima... \n");
+         }
+            printf("Esquerda... \n");
+         }
+        break;
+        }
+        printf("\n\n\n");
         
         
         
